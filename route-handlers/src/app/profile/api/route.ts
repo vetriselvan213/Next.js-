289ac595e29535b4d8a51c090fc369eb6cba7391
s vetriselvan <122189457+vetriselvan213@users.noreply.github.com>
2024-07-03 13:24:13 +0530

    route handlers commit

---
+++ route-handlers/src/app/profile/api/route.ts
@@ -0,0 +1,23 @@
+import { cookies, headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers();
+
+    cookies().set("resultsPerPage", "2");
+    const theme = request.cookies.get("theme");
+
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"))
+    console.log(theme)
+    console.log(cookies().get("resultsPerPage")) 
+
+    return new Response("<h1>Profile API data</h1>",{
+        headers: {
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark",
+        },
+    });
+}
